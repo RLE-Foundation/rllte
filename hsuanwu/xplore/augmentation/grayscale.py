e58246d8b7c3bdcc43b5490312fe1abe34e62e89
Yuanmo <friedrichyuan19990827@gmail.com>
2023-03-29 09:48:50 +0800

    Update grayscale.py

--- hsuanwu/xplore/augmentation/grayscale.py
+++ hsuanwu/xplore/augmentation/grayscale.py
@@ -18,7 +18,7 @@ class GrayScale(BaseAugmentation):
         super().__init__()
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        b, c, h, w = x.shape
+        b, c, h, w = x.size()
         frames = c // 3
         x = x.view([b, frames, 3, h, w])
         x = 0.2989 * x[:, :, 0, ...] + 0.5870 * x[:, :, 1, ...] + 0.1140 * x[:, :, 2, ...]
