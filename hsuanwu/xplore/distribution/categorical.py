14a26c069c502b824e33d6a82b4badb69c73f494
yuanmingqi <friedrichyuan19990827@gmail.com>
2023-03-31 23:25:11 +0800

    udpate dist

--- hsuanwu/xplore/distribution/categorical.py
+++ hsuanwu/xplore/distribution/categorical.py
@@ -1,26 +1,39 @@
 import torch.distributions as pyd
 
-from hsuanwu.common.typing import *
+from hsuanwu.common.typing import Tensor, TorchSize
+from hsuanwu.xplore.distribution import BaseDistribution
 
 
-class Categorical(pyd.Categorical):
+class Categorical(BaseDistribution):
     """Categorical distribution for sampling actions in discrete control tasks.
     Args:
-        logits: event log probabilities (unnormalized).
+        mu (Tensor): mean of the distribution. For Categorical distribution, mu denotes the "logits": event log probabilities (unnormalized).
+        sigma (Tensor): Deprecated here.
+        low (float): Deprecated here.
+        high (float): Deprecated here.
+        eps (float): Deprecated here.
 
     Returns:
         Categorical distribution instance.
     """
 
-    def __init__(self, logits=None):
-        super().__init__(probs=None, logits=logits, validate_args=None)
+    def __init__(
+        self,
+        mu: Tensor,
+        sigma: None,
+        low: None,
+        high: None,
+        eps: None,
+    ) -> None:
+        super().__init__(mu, sigma, low, high, eps)
+        self.dist = pyd.Categorical(logits=mu)
 
     def sample(self):
-        return super().sample().unsqueeze(-1)
+        return self.dist.sample().unsqueeze(-1)
 
     def log_probs(self, actions) -> Tensor:
         return (
-            super()
+            self.dist
             .log_prob(actions.squeeze(-1))
             .view(actions.size(0), -1)
             .sum(-1)
@@ -29,4 +42,4 @@ class Categorical(pyd.Categorical):
 
     @property
     def mean(self) -> Tensor:
-        return self.probs.argmax(dim=-1, keepdim=True)
+        return self.dist.probs.argmax(dim=-1, keepdim=True)
