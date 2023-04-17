bfa8a9f5870c6bc64c69bf3d200d6845cab9f31d
Tarnished <friedrichyuan19990827@gmail.com>
2023-04-17 19:13:43 +0800

    update cfgs

--- train.py
+++ train.py
@@ -1,15 +1,15 @@
-import hydra
+import hydra # Use Hydra to manage experiments
 
-from hsuanwu.env import make_dmc_env
-from hsuanwu.common.engine import OffPolicyTrainer
+from hsuanwu.env import make_dmc_env # Import DeepMind Control Suite
+from hsuanwu.common.engine import HsuanwuEngine # Import Hsuanwu engine
 
-train_env = make_dmc_env(env_id='cartpole_balance')
-test_env = make_dmc_env(env_id='cartpole_balance')
+train_env = make_dmc_env(env_id='cartpole_balance') # Create train env
+test_env = make_dmc_env(env_id='cartpole_balance') # Create test env
 
-@hydra.main(version_base=None, config_path='cfgs', config_name='config')
+@hydra.main(version_base=None, config_path='cfgs', config_name='minimum_config')
 def main(cfgs):
-    trainer = OffPolicyTrainer(train_env=train_env, test_env=test_env, cfgs=cfgs)
-    trainer.train()
+    engine = HsuanwuEngine(cfgs=cfgs, train_env=train_env, test_env=test_env) # Initialize engine
+    engine.invoke() # Start training
 
 if __name__ == '__main__':
     main()
