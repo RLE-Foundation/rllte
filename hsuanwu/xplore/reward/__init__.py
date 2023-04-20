6bb4152a235a6d2157dcf48811d82d9278d1a4c1
Tarnished <friedrichyuan19990827@gmail.com>
2023-04-20 23:16:10 +0800

    update ride

--- hsuanwu/xplore/reward/__init__.py
+++ hsuanwu/xplore/reward/__init__.py
@@ -1,10 +1,10 @@
 from .base import BaseIntrinsicRewardModule as BaseIntrinsicRewardModule
 from .pseudo_counts import PseudoCounts as PseudoCounts
 # from .girm import GIRM as GIRM
-# from .icm import ICM as ICM
+from .icm import ICM as ICM
 from .ngu import NGU as NGU
 # from .re3 import RE3 as RE3
 # from .revd import REVD as REVD
-# from .ride import RIDE as RIDE
+from .ride import RIDE as RIDE
 # from .rise import RISE as RISE
 from .rnd import RND as RND
