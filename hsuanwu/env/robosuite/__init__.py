c9aca750db238c25c2bd5325aa2e503fd14ebbf0
Tarnished <friedrichyuan19990827@gmail.com>
2023-05-15 14:33:36 +0800

    update env

--- hsuanwu/env/robosuite/__init__.py
+++ hsuanwu/env/robosuite/__init__.py
@@ -1,81 +1,101 @@
-from typing import Callable, Dict, List, Any, Tuple
+from typing import Callable, Dict, Tuple
+
 import gymnasium as gym
+import robosuite as suite
+from robosuite.wrappers import GymWrapper
 import numpy as np
-from collections import deque
-import torch as th
-from hsuanwu.env.utils import TorchVecEnvWrapper
-from gymnasium.vector import SyncVectorEnv
+from gymnasium.vector import AsyncVectorEnv, SyncVectorEnv
 from gymnasium.wrappers import RecordEpisodeStatistics
 
-class PixelEnv(gym.Env):
-    """Observation wrapper that stacks the observations in a rolling manner.
+from hsuanwu.env.utils import TorchVecEnvWrapper
+
+
+class AdapterEnv(gym.Wrapper):
+    """PyBullet robotics envs currently doesn't support Gymnasium.
 
     Args:
         env (Env): Environment to wrap.
-        k (int): Number of stacked frames.
 
     Returns:
-        FrameStackEnv instance.
+        AdapterEnv instance.
     """
 
-    def __init__(self) -> None:
-        self._k = 3
-        self._frames = deque([], maxlen=3)
+    def __init__(self, env: gym.Env) -> None:
+        super().__init__(env)
         self.observation_space = gym.spaces.Box(
-            low=0,
-            high=255,
-            shape=(9, 84, 84),
-            dtype=np.uint8
+            shape=env.observation_space.shape,
+            dtype=env.observation_space.dtype,
+            low=env.observation_space.low,
+            high=env.observation_space.high,
         )
         self.action_space = gym.spaces.Box(
-            low=-1.0,
-            high=1.0,
-            shape=(1, ),
-            dtype=np.float32
+            shape=env.action_space.shape,
+            dtype=env.action_space.dtype,
+            low=env.action_space.low,
+            high=env.action_space.high,
         )
-        self.total_step = 0
 
-    def reset(self, **kwargs) -> Tuple[th.Tensor, Dict]:
-        self.total_step = 0
-        obs = self.observation_space.sample()[:3,:,:]
-        info = {'discount': 1.0}
+    def step(self, action: int) -> Tuple[np.ndarray, float, bool, bool, Dict]:
+        obs, reward, done, info = self.env.step(action)
+        return obs, reward, done, done, info
 
-        for _ in range(self._k):
-            self._frames.append(obs)
-        return self._get_obs(), info
+    def reset(self, **kwargs) -> Tuple[np.ndarray, Dict]:
+        obs = self.env.reset()
+        return obs, {}
 
-    def step(self, action: Tuple[float]) -> Tuple[Any, float, bool, bool, Dict]:
-        # obs, reward, terminated, truncated, info = self.env.step(action)
-
-        obs = self.observation_space.sample()[:3,:,:]
-        reward = np.random.rand(1)
-        if self.total_step < 499:
-            terminated = truncated = False
-        else:
-            terminated = truncated = True
-        info = {'discount': 1.0}
-        self.total_step += 1
-
-        self._frames.append(obs)
-        return self._get_obs(), reward, terminated, truncated, info
-
-    def _get_obs(self) -> np.ndarray:
-        assert len(self._frames) == self._k
-        return np.concatenate(list(self._frames), axis=0)
-    
 
 def make_robosuite_env(
-        env_id: str = "cartpole_balance",
-        num_envs: int = 1,
-        device: str = "cpu"):
-    def make_env():
+    env_id: str = "Lift_Panda", 
+    num_envs: int = 1, 
+    device: str = "cpu", 
+    seed: int = 0, 
+    distributed: bool = False,
+    has_renderer: bool = False,
+    has_offscreen_renderer: bool = False,
+    use_camera_obs: bool = False,
+) -> gym.Env:
+    """Build PyBullet robotics environments.
+
+    Args:
+        env_id (str): Name of environment.
+        num_envs (int): Number of environments.
+        device (str): Device (cpu, cuda, ...) on which the code should be run.
+        seed (int): Random seed.
+        distributed (bool): For `Distributed` algorithms, in which `SyncVectorEnv` is required
+            and reward clip will be used before environment vectorization.
+        has_renderer (bool): If true, render the simulation state in
+            a viewer instead of headless mode.
+        has_offscreen_renderer (bool): True if using off-screen rendering.
+        use_camera_obs (bool): True for using image observations.
+
+    Returns:
+        Environments instance.
+    """
+
+    def make_env(env_id: str, seed: int) -> Callable:
         def _thunk():
-            return PixelEnv()
+            env_name, robots = env_id.split("_")
+            env = suite.make(
+                env_name=env_name,
+                robots=robots,
+                has_renderer=has_renderer,
+                has_offscreen_renderer=has_offscreen_renderer,
+                use_camera_obs=use_camera_obs
+            )
+            env = GymWrapper(env)
+            env.seed(seed)
+            env = AdapterEnv(env)
+            env.observation_space.seed(seed)
+            env.action_space.seed(seed)
+            return env
 
         return _thunk
 
-    envs = [make_env() for i in range(num_envs)]
-    envs = SyncVectorEnv(envs)
-    envs = RecordEpisodeStatistics(envs)
+    envs = [make_env(env_id, seed + i) for i in range(num_envs)]
+    if distributed:
+        envs = SyncVectorEnv(envs)
+    else:
+        envs = AsyncVectorEnv(envs)
+        envs = RecordEpisodeStatistics(envs)
 
-    return TorchVecEnvWrapper(envs, device)
+    return TorchVecEnvWrapper(envs, device=device)
