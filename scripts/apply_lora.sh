b821b241d550f6888a98267d30c651a2d77266cb
yuanmingqi <friedrichyuan19990827@gmail.com>
2023-06-28 20:28:42 +0800

    update lora

---
+++ scripts/apply_lora.sh
@@ -0,0 +1,4 @@
+python -m rllte.copilot.apply_lora \
+	--base-model-path /export/yuanmingqi/code/LLM/vicuna-7b-v1.3 \
+	--target-model-path /export/yuanmingqi/code/LLM/rllte-vicuna-7b \
+	--lora-path /export/yuanmingqi/code/LLM/rllte_lora_checkpoints
--- scripts/copilot.sh
+++ scripts/copilot.sh
@@ -1,4 +1,4 @@
 python -m rllte.copilot.controller &
-python -m rllte.copilot.model_worker --model-path ../LLM/rllte-vicuna-7b-v1.3 &
+python -m rllte.copilot.model_worker --model-path ../LLM/rllte-vicuna-7b &
 sleep 20
 python -m rllte.copilot.server
