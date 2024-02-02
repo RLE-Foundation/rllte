7e8b1b1d84a0f0d700208558a867752752e1471b
roger <creus99@gmail.com>
2024-02-02 15:24:51 -0500

    wip

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def parse_args():
     parser.add_argument("--clip_range", type=float, default=0.1)
     parser.add_argument("--clip_range_vf", type=float, default=0.1)
     parser.add_argument("--vf_coef", type=float, default=0.5)
-    parser.add_argument("--ent_coef", type=float, default=0.01)
+    parser.add_argument("--ent_coef", type=float, default=0.0)
     parser.add_argument("--max_grad_norm", type=float, default=0.5)
     parser.add_argument("--discount", type=float, default=0.99)
     parser.add_argument("--init_fn", type=str, default="orthogonal")
