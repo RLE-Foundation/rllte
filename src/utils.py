06de84b2ef3c8dc0a6da4bf9448fbd5dc25b7e16
roger-creus <creus99@gmail.com>
2024-02-14 10:37:35 -0500

    wip

--- src/utils.py
+++ src/utils.py
@@ -112,10 +112,11 @@ def make_env(args, device):
             env_id=args.env_id,
             num_envs=args.n_envs,
             device=device,
-            #num_levels=1,
-            #start_level=495,
-            #seed=394,
+            num_levels=1,
+            start_level=495,
+            seed=394,
         )
+
     elif "MiniGrid" in args.env_id:
         from rllte.env import make_minigrid_env
         env = make_minigrid_env(
