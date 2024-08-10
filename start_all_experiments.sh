09caacd367841960ae5506fb0db1ced3ab65bba4
roger-creus <creus99@gmail.com>
2024-08-10 18:28:51 -0400

    wip

--- start_all_experiments.sh
+++ start_all_experiments.sh
@@ -9,10 +9,10 @@ for seed in {1..3}; do
     for reward in "${intrinsic_rewards[@]}"; do
         # Run with --obs_rms
         echo "Running with seed=${seed}, intrinsic_reward=${reward}, and obs_rms"
-        python src/ppo.py --env_id=MiniHack-Room-5x5-v0 --intrinsic_reward=${reward} --seed=${seed} --obs_rms
+        python src/ppo.py --env_id=MiniHack-MazeWalk-15x15-v0 --intrinsic_reward=${reward} --seed=${seed} --obs_rms
 
         # Run without --obs_rms
         echo "Running with seed=${seed}, intrinsic_reward=${reward}, and without obs_rms"
-        python src/ppo.py --env_id=MiniHack-Room-5x5-v0 --intrinsic_reward=${reward} --seed=${seed}
+        python src/ppo.py --env_id=MiniHack-MazeWalk-15x15-v0 --intrinsic_reward=${reward} --seed=${seed}
     done
 done
