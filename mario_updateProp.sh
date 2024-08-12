6637576b05c342e1bed55a85384f4bd6b6464e09
roger <creus99@gmail.com>
2024-08-11 22:42:17 -0400

    wip

--- mario_updateProp.sh
+++ mario_updateProp.sh
@@ -1,65 +1,65 @@
 # BASELINE 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=icm --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=icm --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=icm --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=icm --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=icm --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=icm --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=e3b --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=e3b --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=e3b --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=e3b --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=e3b --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=e3b --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=rnd --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=rnd --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=rnd --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=rnd --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=rnd --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=rnd --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=pseudocounts --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=pseudocounts --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=pseudocounts --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=pseudocounts --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=pseudocounts --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=pseudocounts --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ngu --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ngu --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ngu --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ngu --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ngu --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ngu --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=re3 --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=re3 --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=re3 --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=re3 --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=re3 --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=re3 --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ride --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ride --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=ride --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ride --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ride --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=ride --seed=3
 
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=disagreement --seed=1
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=disagreement --seed=2
-sbatch train_long --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.01 --intrinsic_reward=disagreement --seed=3
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=disagreement --seed=1
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=disagreement --seed=2
+sbatch train_long --pretraining --obs_rms --env_id=SuperMarioBros-1-1-v3 --update_proportion=0.001 --intrinsic_reward=disagreement --seed=3
 
 # Q1 OBS_RMS 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=icm --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=icm --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=icm --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=icm --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=icm --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=icm --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=e3b --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=e3b --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=e3b --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=e3b --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=e3b --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=e3b --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=rnd --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=rnd --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=rnd --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=rnd --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=rnd --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=rnd --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=pseudocounts --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=pseudocounts --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=pseudocounts --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=pseudocounts --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=pseudocounts --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=pseudocounts --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ngu --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ngu --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ngu --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ngu --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ngu --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ngu --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=re3 --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=re3 --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=re3 --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=re3 --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=re3 --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=re3 --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ride --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ride --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=ride --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ride --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ride --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=ride --seed=3
 
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=disagreement --seed=1
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=disagreement --seed=2
-sbatch train_long --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=10.0 --intrinsic_reward=disagreement --seed=3
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=disagreement --seed=1
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=disagreement --seed=2
+sbatch train_long --pretraining --obs_rms  --env_id=SuperMarioBros-1-1-v3 --update_proportion=100.0 --intrinsic_reward=disagreement --seed=3
